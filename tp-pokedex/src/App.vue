b49e064f58171530995c7defa760870738c08f44
Solweg <lngouguenheim@hotmail.fr>
2026-01-09 14:06:04 +0100

    récupération les données des Pokémons depuis l'API Tyradex

--- tp-pokedex/src/App.vue
+++ tp-pokedex/src/App.vue
@@ -1,11 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
+  <h1>Mon Pokédex du Turfu en Vue.js</h1>
+  <p v-if="pokemons.length === 0">Chargement des petits animaux mignons...</p>
+    <ul>
+      <li v-for="pokemon in pokemons" :key="pokemon.pokedex_id">
+      {{ pokemon.name.fr }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<style scoped>
-</style>
+<script setup>
+  import {ref,onMounted} from 'vue'
+
+  const pokemons = ref([]) // Variable réactive pour stocker les pokémons
+
+  onMounted(async () => {
+    try {
+      // Appel l'API Tyradex
+      const response = await fetch ('ttps://tyradex.vercel.app/api/v1/pokemon')
+      const data = await response.json()
+
+      // Met les données dans la variable pokemons
+      pokemons.value = data
+    }catch (error) {
+      console.error("Erreur lors de l'appel API:", error)
+    }
+    }
+  )
+</script>
