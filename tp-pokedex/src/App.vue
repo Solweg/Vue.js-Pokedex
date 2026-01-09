142083ed17337bf9087460b528b75db5b4ea166a
Solweg <lngouguenheim@hotmail.fr>
2026-01-09 14:59:07 +0100

    suppression de l'element 0

--- tp-pokedex/src/App.vue
+++ tp-pokedex/src/App.vue
@@ -2,35 +2,26 @@
   <Header />
 
   <main>
-    
+
     <div class="container">
-      <PokeCard 
-        v-for="p in pokemons" 
-        :key="p.pokedex_id" 
-        :pokemon="p" 
-        @click="openModal(p)"
-      />
+      <PokeCard v-for="p in pokemons" :key="p.pokedex_id" :pokemon="p" @click="openModal(p)" />
     </div>
 
-    <PokeModal 
-      v-if="selectedPokemon" 
-      :pokemon="selectedPokemon" 
-      @close="selectedPokemon = null" 
-    />
+    <PokeModal v-if="selectedPokemon" :pokemon="selectedPokemon" @close="selectedPokemon = null" />
 
-    </main>
+  </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import Header from './components/Header.vue'
 import PokeCard from './components/PokeCard.vue'
-import PokeModal from './components/PokeModal.vue' // AJOUT : Il manquait l'import de la modale ici
+import PokeModal from './components/PokeModal.vue'
 
 const pokemons = ref([])
 const selectedPokemon = ref(null) // Contient le Pokémon à afficher dans la modale
 
-//Ouvre la modale
+
 const openModal = (pokemon) => {
   selectedPokemon.value = pokemon
   console.log("On veut voir :", pokemon.name.fr)
@@ -40,6 +31,7 @@ onMounted(async () => {
   try {
     const response = await fetch('https://tyradex.vercel.app/api/v1/pokemon')
     const data = await response.json()
+    data.shift()
     pokemons.value = data
   } catch (error) {
     console.error("Erreur :", error)
@@ -53,6 +45,7 @@ body {
   font-family: sans-serif;
   background-color: #f5f5f5;
 }
+
 .container {
   display: flex;
   flex-wrap: wrap;
@@ -60,6 +53,7 @@ body {
   justify-content: center;
   padding: 20px;
 }
+
 .loader {
   text-align: center;
   font-weight: bold;
